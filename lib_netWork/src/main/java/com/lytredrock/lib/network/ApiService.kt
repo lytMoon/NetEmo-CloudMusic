3a580bcb43a7bdad34e058713f4116e84c6de71f
lytMoon <2936437824@qq.com>
2023-07-14 22:41:25 +0800

    成功进行了网络请求的测试

--- lib_netWork/src/main/java/com/lytredrock/lib/network/ApiService.kt
+++ lib_netWork/src/main/java/com/lytredrock/lib/network/ApiService.kt
@@ -2,10 +2,10 @@ package com.lytredrock.lib.network
 
 import com.lytredrock.lib.network.musicData.Comment
 import com.lytredrock.lib.network.musicData.MusicComment
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 /**
  * description ：数据接收类
@@ -17,7 +17,7 @@ import retrofit2.http.Path
 interface ApiService {
      //   示例：comment/music?id=186016&limit=1
      @GET("comment/music")
-     fun getMusicComments(@Path("id") id:String,@Path("limit")limit: Int =100): Observable<MusicComment<Comment>>
+     fun getMusicComments(@Query("id") id:Int,@Query("limit")limit: Int =100): Call<MusicComment<Comment>>
 
 
 }
--- lib_netWork/src/main/java/com/lytredrock/lib/network/ViewModel.kt
+++ lib_netWork/src/main/java/com/lytredrock/lib/network/ViewModel.kt
@@ -6,14 +6,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.lytredrock.lib.network.musicData.Comment
 import com.lytredrock.lib.network.musicData.MusicComment
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.core.SingleObserver
-import io.reactivex.rxjava3.disposables.Disposable
-import io.reactivex.rxjava3.schedulers.Schedulers
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * description ：在ViewModel中封装了网络请求的具体逻辑
@@ -22,45 +18,60 @@ import retrofit2.converter.gson.GsonConverterFactory
  * date : 2023/7/14 17:28
  * version: 1.0
  */
-class ViewModel {
-    //把retrofit对象和apiService对象的构造先提取出来
-    val retrofit = Retrofit.Builder()
-        .baseUrl("http://why.vin:2023/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-        .build()
-    val apiService= retrofit.create(ApiService::class.java)
+class ViewModel:androidx.lifecycle.ViewModel() {
+
+    val apiService= ServiceCreator.create(ApiService::class.java)
 
     private val _musicComments: MutableLiveData<List<Comment>> = MutableLiveData()
     val musicComments: LiveData<List<Comment>>
         get() = _musicComments
 
     /***
-     * 获取音乐信息的评论，这里接受一个id参数，（指定了评论获取50条，如果影响效果后面再改）
+     * 获取音乐信息的评论，这里接受一个id参数，（指定了评论获取100条，如果影响效果后面再改）
      */
     @SuppressLint("CheckResult")
-    fun receiveMusicComments(id:String){
+    fun receiveMusicComments(id:Int){
        apiService.getMusicComments(id)
-           .subscribeOn(Schedulers.newThread())//新开一个线程进行请求
-           .observeOn(AndroidSchedulers.mainThread())//在安卓主线程更新UI
-           .subscribe(object : Observer<MusicComment<Comment>>{
-               override fun onSubscribe(d: Disposable) {
+           .enqueue(object: Callback<MusicComment<Comment>>{
+               override fun onResponse(
+                   call: Call<MusicComment<Comment>>,
+                   response: Response<MusicComment<Comment>>
+
+               ) {
+                   _musicComments.postValue(response.body()?.comments)
+                   val list = response.body()?.comments
+                   if (list != null) {
+                       for (it in list)
+                           Log.d("receiveMusicComments","(ViewModel.kt:45)-->> "+it.content)
+                   };
+
                }
 
-               override fun onError(e: Throwable) {
-               }
-
-               override fun onComplete() {
-               }
-
-               override fun onNext(t: MusicComment<Comment>) {
-                   _musicComments.postValue(t.comments)
-                   Log.d("receiveMusicComments", "(ViewModel.kt:57)-->> $_musicComments");
-
-
+               override fun onFailure(call: Call<MusicComment<Comment>>, t: Throwable) {
+                  Log.d("receiveMusicComments", "(ViewModel.kt:57)-->> 失败了");
                }
 
            })
+//           .subscribeOn(Schedulers.newThread())//新开一个线程进行请求
+//           .observeOn(AndroidSchedulers.mainThread())//在安卓主线程更新UI（执行onNext的逻辑）
+//           .subscribe(object : Observer<MusicComment<Comment>> {
+//               override fun onSubscribe(d: Disposable) {
+//               }
+//
+//               override fun onError(e: Throwable) {
+//               }
+//
+//               override fun onComplete() {
+//               }
+//
+//               override fun onNext(t: MusicComment<Comment>) {
+//                   _musicComments.postValue(t.comments)
+//                   Log.d("receiveMusicComments", "(ViewModel.kt:57)-->> $_musicComments");
+//
+//
+//               }
+//
+//           })
 
    }
 
