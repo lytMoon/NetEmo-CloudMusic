1cf90f6641b5bbe0ce13ede95a6cb0994f5fbcc3
古云顿 <2191288460@qq.com>
2023-07-15 15:34:01 +0800

    实现了splash闪屏页面

--- lib_base/src/main/java/com/lytredrock/lib/base/BaseActivity.kt
+++ lib_base/src/main/java/com/lytredrock/lib/base/BaseActivity.kt
@@ -3,6 +3,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
+import android.os.Bundle
 import android.widget.Toast
 import android.view.View
 import android.view.Window
