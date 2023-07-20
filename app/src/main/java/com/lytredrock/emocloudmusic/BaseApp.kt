37c31039c5a053f1ec7cff6874c160a547240d44
lytMoon <2936437824@qq.com>
2023-07-20 12:37:38 +0800

    :zap: 搭建好了arouter框架，初步了解了多模块开发的各个模块如何通信的关系（一点点），可以使用arouter进行跳转

---
+++ app/src/main/java/com/lytredrock/emocloudmusic/BaseApp.kt
@@ -0,0 +1,21 @@
+package com.lytredrock.emocloudmusic
+
+import android.app.Application
+
+/**
+ * ...
+ * @author RQ527 (Ran Sixiang)
+ * @email 1799796122@qq.com
+ * @date 2023/7/14
+ * @Description:
+ */
+open class BaseApp:Application() {
+    companion object {
+        lateinit var mContext: BaseApp
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mContext=this
+    }
+}
---
+++ model_research/src/main/java/com/lytredrock/model/research/Arouter.kt
@@ -0,0 +1,10 @@
+package com.lytredrock.model.research
+
+/**
+ * description ：
+ * author : lytMoon
+ * email : yytds@foxmail.com
+ * date : 2023/7/20 11:27
+ * version: 1.0
+ */
+const val TEST_SERVICE = "/test/service"
